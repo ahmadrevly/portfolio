94ee999b62025aad46a5aa73339422b4967a010c
Revly <ahmadrevlyyy@gmail.com>
2024-12-14 16:30:44 +0700

    update

--- out/script.js
+++ out/script.js
@@ -3,11 +3,16 @@
 window.onscroll = function () {
   const header = document.querySelector("header");
   const fixedNav = header.offsetTop;
+  const toTop = document.querySelector("#top");
 
   if (window.pageYOffset > fixedNav) {
     header.classList.add("navbar-fixed");
+    toTop.classList.remove("hidden");
+    toTop.classList.add("flex");
   } else {
     header.classList.remove("navbar-fixed");
+    toTop.classList.remove("remove");
+    toTop.classList.add("hidden");
   }
 };
 
@@ -19,4 +24,25 @@ const navMenu = document.querySelector("#nav-menu");
 hamburger.addEventListener("click", function () {
   hamburger.classList.toggle("hamburger-active");
   navMenu.classList.toggle("hidden");
+});
+
+// Klik di luar hamburger
+
+window.addEventListener("click", function (e) {
+  if (e.target != hamburger && e.target != navMenu) {
+    hamburger.classList.toggle("hamburger-active");
+    navMenu.classList.toggle("hidden");
+  }
+});
+
+// Dark toggle
+const darkToggle = document.querySelector("#dark-toggle");
+const html = document.querySelector("html");
+
+darkToggle.addEventListener("click", function () {
+  if (darkToggle.checked) {
+    html.classList.add("dark");
+  } else {
+    html.classList.remove("dark");
+  }
 });
